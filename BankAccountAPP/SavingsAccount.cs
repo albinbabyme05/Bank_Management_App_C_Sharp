5f85a4857dda87b4593d29b5794bc70849b8cb64
Albin Baby <albinbabayme05@gmail.com>
2025-04-03 11:42:40 +0200

    ValidateDeposit method set as virtual in BankAccount - why ?->if there is SavingsAccount it will override the BankAccount Class

--- BankAccountAPP/SavingsAccount.cs
+++ BankAccountAPP/SavingsAccount.cs
@@ -12,10 +12,34 @@ namespace BankAccountAPP
         public decimal InterestRate { get; set; }
 
         //create constructor for SavingsAccount Class
-        public SavingsAccount(string owner, decimal interestRate) : base(owner)
+        /*when the SavingsAccount Constructor is initialized, the BankAccout Constructor also invoked that explained below code*/
+        public SavingsAccount(string owner, decimal interestRate) : base(owner + "(S)")
         {
             InterestRate = interestRate;
         }
 
+        public decimal CalcualteInterestRate(decimal amountToInterested)
+        {
+            return (amountToInterested * InterestRate) / 100;
+            
+        }
+
+        public override string ValidateDeposit(decimal amount)
+        {
+            if (amount > 20000)
+            {
+                return "Deposit Limit reached. Please Contact BM.";
+            }
+            else if (amount <= 0)
+            {
+                return $"You can not deposit {amount} Euro";
+            }
+            else
+            {
+                decimal rate = CalcualteInterestRate(amount);
+                Balance += amount + rate;
+                return $"{amount} Euro Successfully Deposited";
+            }
+        }
     }
 }
